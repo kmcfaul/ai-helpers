2c519ed57187f114bf85ea5e7a1afae7cc57a58a
Katie McFaul <kmcfaul@redhat.com>
2025-10-16 14:29:00 -0400

    add ai exp icon

--- src/components/Compass.tsx
+++ src/components/Compass.tsx
@@ -15,6 +15,7 @@ import {
   DropdownList,
   MenuToggleElement,
   Flex,
+  ActionListGroup,
 } from "@patternfly/react-core";
 import { MessageBar } from "@patternfly/chatbot";
 import PlayIcon from "@patternfly/react-icons/dist/esm/icons/play-icon";
@@ -26,6 +27,30 @@ import imgAvatar from "../assets/avatarImg.svg?url";
 import { CompassIntegrations } from "./CompassIntegrations";
 import { CompassAutomations } from "./CompassAutomations";
 import { BrandLogo } from "./BrandLogo";
+
+const aiExperienceIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    width="1em"
+    height="1em"
+    viewBox="0 0 32 32"
+  >
+    <g id="uuid-75f76c23-c1b6-4d16-98c6-ad548b061af4">
+      <rect width="32" height="32" fill="none" />
+    </g>
+    <g id="uuid-08df3350-7f5a-4f9c-8ddc-ec9ca74ef47e">
+      <path
+        fill="var(--pf-t--global--icon--color--regular)"
+        d="M26.03711,16.96191c-5.9043-.46826-10.53027-5.09424-10.99902-10.99121-.03711-.52344-.51367-.96973-1.03809-.96973-.52148,0-.99707.44189-1.03809.96191-.46875,5.90479-5.09473,10.53076-10.99121,10.99854-.52344.0376-.96973.51416-.96973,1.03857,0,.52148.44238.99707.96191,1.03809,5.9043.46875,10.53027,5.09473,10.99902,10.99121.03711.52344.51367.96973,1.03809.96973.52148,0,.99707-.44238,1.03809-.96191.46875-5.9043,5.09473-10.53027,10.99902-10.99902.51953-.04199.96191-.51562.96191-1.03809,0-.52148-.44238-.99707-.96191-1.03809Z"
+      />
+      <path
+        fill="var(--pf-t--global--icon--color--regular)"
+        d="M30.06836,6.14746c-1.59668-.35693-2.85938-1.61914-3.21582-3.21387-.08887-.40332-.43945-.68555-.85254-.68555s-.76367.28223-.85156.68311c-.35742,1.59717-1.62012,2.85938-3.21973,3.2168-.40039.09131-.68066.44141-.68066.85205s.28027.76074.68359.85254c1.59668.35693,2.85938,1.61914,3.21582,3.21387.08887.40332.43945.68555.85254.68555s.76367-.28223.85156-.68311c.35742-1.59717,1.62012-2.85938,3.2168-3.21631.00098,0,.00195-.00049.00293-.00049.40039-.09131.68066-.44141.68066-.85205s-.28027-.76074-.68359-.85254Z"
+      />
+    </g>
+  </svg>
+);
 
 export const Compass: React.FunctionComponent = () => {
   const [activeSection, setActiveSection] = useState(4);
@@ -75,7 +100,10 @@ export const Compass: React.FunctionComponent = () => {
           isExpanded={isDropdownOpen}
           variant="plain"
         >
-          <Flex alignItems={{ default: 'alignItemsCenter' }} gap={{ default: 'gapMd' }}>
+          <Flex
+            alignItems={{ default: "alignItemsCenter" }}
+            gap={{ default: "gapMd" }}
+          >
             Ned Username
             <Avatar src={imgAvatar} alt="" size="md" />
           </Flex>
@@ -161,18 +189,29 @@ export const Compass: React.FunctionComponent = () => {
       </div>
       <div id="pf-compass__west" className="compass__panel--start">
         <ActionList className="pf-m-vertical">
+          <ActionListGroup>
+            <ActionListItem>
+              <Button variant="plain" icon={<PlayIcon />} />
+            </ActionListItem>
+            <ActionListItem>
+              <Button variant="plain" icon={<OutlinedPlusSquare />} />
+            </ActionListItem>
+          </ActionListGroup>
           <ActionListItem>
-            <Button variant="plain" icon={<PlayIcon />} />
+            <Button
+              className="compass__sparkle"
+              variant="plain"
+              icon={aiExperienceIcon}
+            ></Button>
           </ActionListItem>
-          <ActionListItem>
-            <Button variant="plain" icon={<OutlinedPlusSquare />} />
-          </ActionListItem>
-          <ActionListItem>
-            <Button variant="plain" icon={<OutlinedQuestionCircleIcon />} />
-          </ActionListItem>
-          <ActionListItem>
-            <Button variant="plain" icon={<OutlinedCopy />} />
-          </ActionListItem>
+          <ActionListGroup>
+            <ActionListItem>
+              <Button variant="plain" icon={<OutlinedQuestionCircleIcon />} />
+            </ActionListItem>
+            <ActionListItem>
+              <Button variant="plain" icon={<OutlinedCopy />} />
+            </ActionListItem>
+          </ActionListGroup>
         </ActionList>
       </div>
       <div id="pf-compass__main" className="compass__main">
