7220cd4e6676ce64eac6652748793be0d72d5413
Katie McFaul <kmcfaul@redhat.com>
2025-10-17 15:40:09 -0400

    add compass wrapper to automations page

--- src/components/CompassAutomations.tsx
+++ src/components/CompassAutomations.tsx
@@ -12,7 +12,7 @@ import {
   Button,
 } from "@patternfly/react-core";
 import EllipsisVIcon from "@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon";
-import { CompassAutomationTopology } from '../automationTopology/CompassAutomationTopology.tsx';
+import { CompassAutomationTopology } from "../automationTopology/CompassAutomationTopology.tsx";
 
 export const CompassAutomations: React.FunctionComponent = () => {
   return (
@@ -22,28 +22,32 @@ export const CompassAutomations: React.FunctionComponent = () => {
         className="pf-m-no-sidebar pf-m-plain"
         isContentFilled
       >
-        <PageSection>
-          <Flex alignItems={{ default: "alignItemsCenter" }}>
-            <FlexItem grow={{ default: "grow" }}>
-              <Title headingLevel="h1">Automations</Title>
-            </FlexItem>
-            <FlexItem>
-              <Toolbar hasNoPadding>
-                <ToolbarContent>
-                  <ToolbarGroup></ToolbarGroup>
-                  <ToolbarGroup>
-                    <ToolbarItem>
-                      <Button icon={<EllipsisVIcon />} variant="plain" />
-                    </ToolbarItem>
-                  </ToolbarGroup>
-                </ToolbarContent>
-              </Toolbar>
-            </FlexItem>
-          </Flex>
-        </PageSection>
-        <PageSection isFilled hasBodyWrapper={false}>
-          <CompassAutomationTopology />
-        </PageSection>
+        <div className="compass__toolbar">
+          <PageSection>
+            <Flex alignItems={{ default: "alignItemsCenter" }}>
+              <FlexItem grow={{ default: "grow" }}>
+                <Title headingLevel="h1">Automations</Title>
+              </FlexItem>
+              <FlexItem>
+                <Toolbar hasNoPadding>
+                  <ToolbarContent>
+                    <ToolbarGroup></ToolbarGroup>
+                    <ToolbarGroup>
+                      <ToolbarItem>
+                        <Button icon={<EllipsisVIcon />} variant="plain" />
+                      </ToolbarItem>
+                    </ToolbarGroup>
+                  </ToolbarContent>
+                </Toolbar>
+              </FlexItem>
+            </Flex>
+          </PageSection>
+        </div>
+        <div className="compass__body">
+          <PageSection isFilled hasBodyWrapper={false}>
+            <CompassAutomationTopology />
+          </PageSection>
+        </div>
       </Page>
     </Fragment>
   );
